c4ebd7a88ad24b7e8106344c86534e442b286de0
tsteternlieb <73751684+tsteternlieb@users.noreply.github.com>
2022-01-03 18:30:07 -0500

    more changes

--- Scraping/ScrapeScript/scrape_utils.py
+++ Scraping/ScrapeScript/scrape_utils.py
@@ -1,57 +1,58 @@
-
-import networkx
+# import networkx
 from functools import reduce
 import logging
 import boto3
 from botocore.exceptions import ClientError
 import os
 from queue import Queue
-from threading import Thread 
+from threading import Thread
 import pickle
 import requests
 
-    
 
 def write(data: dict, bucket_name: str, name: str):
-    """function for writing data to s3 buckets st 
-    we get bucket_name/name/fringe for example      
+    """function for writing data to s3 buckets st
+    we get bucket_name/name/fringe for example
 
     Args:
         data (dict): dict of names to data
         bucket_name (str): url of bucket to write to
         name (str): name of current container process
     """
-    s3 = boto3.resource('s3')
-    for k,v in data.items():
-        object = s3.Object(bucket_name, f'{name}/{k}')
+    s3 = boto3.resource("s3")
+    for k, v in data.items():
+        object = s3.Object(bucket_name, f"{name}/{k}")
         b = pickle.dumps(data)
-        object.put(Body = b)
-        
-def getConnectedChannels(slug):
-	#getConnectedChannels(slug) = [c1,c2,...]
-	#slug: channel slug str
-	#[c1,c2,...] where c1,c2,... is linked to the current channel
+        object.put(Body=b)
 
-    GET = f'http://api.are.na/v2/channels/{slug}/connections'
+
+def getConnectedChannels(slug):
+    # getConnectedChannels(slug) = [c1,c2,...]
+    # slug: channel slug str
+    # [c1,c2,...] where c1,c2,... is linked to the current channel
+
+    GET = f"http://api.are.na/v2/channels/{slug}/connections"
     try:
-        collab_json_list = requests.get(GET).json()['channels']
+        collab_json_list = requests.get(GET).json()["channels"]
     except:
         collab_json_list = []
-    slugs = [json['slug'] for json in collab_json_list]
+    slugs = [json["slug"] for json in collab_json_list]
 
     return slugs
+
+
 # print(getConnectedChannels('nynets-_sal8iqlt8y'))
-def generateChannelNetwork(startSlug,depth):
-    '''
-    bfs for network generation 
-    generateChannelNetwork(startSlug,depth) = network : networkx 
-    where nodes represent channels and edges exist if one channels appear in another 
-    '''
+def generateChannelNetwork(startSlug, depth):
+    """
+    bfs for network generation
+    generateChannelNetwork(startSlug,depth) = network : networkx
+    where nodes represent channels and edges exist if one channels appear in another
+    """
     q = Queue()
     seen = set([startSlug])
     q.put(startSlug)
     G = nx.Graph()
-    
+
     while depth > 0:
         for i in range(q.qsize()):
             curr_slug = q.get_nowait()
@@ -60,22 +61,22 @@ def generateChannelNetwork(startSlug,depth):
                 if slug not in seen:
                     seen.add(slug)
                     q.put(slug)
-                        
-                    G.add_edge(curr_slug,slug)
-                    
-                
+
+                    G.add_edge(curr_slug, slug)
+
         depth -= 1
-        
+
     return G
 
-def multi_generateChannelNetwork(startSlugs, num_threads,dic=False, seen = []):
-    def get_connected_channels_from_queue(queue,r):
+
+def multi_generateChannelNetwork(startSlugs, num_threads, dic=False, seen=[]):
+    def get_connected_channels_from_queue(queue, r):
         while True:
             slug = queue.get()
-            slugs = getConnectedChannels(slug)   
+            slugs = getConnectedChannels(slug)
             if dic:
                 if slug not in seen:
-                    r.put((slug,slugs))
+                    r.put((slug, slugs))
             else:
                 for slug in slugs:
                     if slug not in seen:
@@ -86,21 +87,17 @@ def multi_generateChannelNetwork(startSlugs, num_threads,dic=False, seen = []):
     q = Queue()
     for slug in startSlugs:
         q.put(slug)
-    
+
     for i in range(num_threads):
-        worker = Thread(target=get_connected_channels_from_queue, args=(q,res))
+        worker = Thread(target=get_connected_channels_from_queue, args=(q, res))
         worker.setDaemon(True)
         worker.start()
-        
+
     q.join()
     return list(res.queue)
 
-def bfs_channels(name,
-                 bucket_name,
-                 fringe, 
-                 num_threads, 
-                 depth,
-                 dic):
+
+def bfs_channels(name, bucket_name, fringe, num_threads, depth, dic):
     """function for exploring urls in a bfs manner
 
     Args:
@@ -117,33 +114,28 @@ def bfs_channels(name,
         dic_results = []
     for itr in range(depth):
         print(itr)
-        curr_res = multi_generateChannelNetwork(fringe, num_threads,dic, seen)
-        
+        curr_res = multi_generateChannelNetwork(fringe, num_threads, dic, seen)
+
         if dic:
             dic_results.extend(curr_res)
-            
+
             curr_res_keys_ = list(map(lambda x: x[0], curr_res))
-            curr_res_vals_ = list(reduce(lambda x,y: x+y[1],curr_res,[]))
-             
+            curr_res_vals_ = list(reduce(lambda x, y: x + y[1], curr_res, []))
+
             seen.extend(curr_res_keys_)
             fringe = curr_res_vals_
         else:
             fringe = curr_res
     print("1343")
-    
-    dic = {'seen':seen,'dic_results' : dic_results,'fringe' :fringe}
+
+    dic = {"seen": seen, "dic_results": dic_results, "fringe": fringe}
     # print(dic)
-    write(dic,
-          bucket_name,
-          name)
-    
+    write(dic, bucket_name, name)
+
+
 # l = ['nynets-_sal8iqlt8y']
 # (bfs_channels("e","arena-urls",l,50,2,True))
-            
-        
 
-
-    
 
 # def main():
 #     parser = argparse.ArgumentParser(description='startSlug')
@@ -154,7 +146,7 @@ def bfs_channels(name,
 #     print(network)
 
 # if __name__ == "__main__":
-	
+
 # 	import requests
 # 	import math
 # 	import networkx as nx
@@ -162,17 +154,5 @@ def bfs_channels(name,
 # 	from threading import Thread
 # 	import argparse
 # 	print("fart")
-	
+
 # 	main()
-	
-
-
-
-
-
-
-
-
-
-
-
--- Scraping/scrape-deploy.py
+++ Scraping/scrape-deploy.py
@@ -1,49 +1,94 @@
-'''
+"""
 script for deploying containers on aws
-'''
+"""
 import boto3
+import os
+from dotenv import load_dotenv
 
-client = boto3.client('ecs','us-east-1')
-r = client.run_task(
-        taskDefinition='s3ecs',
-        launchType='FARGATE',
-        cluster='scrape-cluster',
-        platformVersion='LATEST',
-)
+load_dotenv()
+# env variables
+SECURITYGROUP = os.getenv("SECURITYGROUP")
 
-def _deploy(start_slug,i: int):
-    """starts a scrape task on ecs
+
+# client = boto3.client("ecs", "us-east-1")
+# r = client.run_task(
+#     taskDefinition="s3ecs",
+#     launchType="FARGATE",
+#     cluster="scrape-cluster",
+#     platformVersion="LATEST",
+# )
+
+
+def _deploy(
+    name: str,
+    start_slug: str,
+    depth: int,
+    dic: bool,
+    num_threads: str,
+    container_name: str,
+    task_def: str,
+):
+    """runs specified task on ecs
 
     Args:
-        start_slug (str): slug to start the crawl from
-        i (int): id of process
+        name (str): name for the task save locations
+        start_slug (str): slug to start crawl at
+        depth (int): depth of bfs to go before completing
+        dic (bool): whether or not to create adjacency dictionary
+        num_threads (str): num of threads, yay multi threading
+        container_name (str): name for the container which we override
+        task_def (str): task name
     """
-    client = boto3.client('ecs')
-    
+
+    client = boto3.client("ecs")
+
     r = client.run_task(
-        taskDefinition='s3ecs',
-        launchType='FARGATE',
-        cluster='scape-cluster',
-        platformVersion='LATEST',
+        taskDefinition=task_def,
+        launchType="FARGATE",
+        cluster="scape-cluster",
+        platformVersion="LATEST",
         count=1,
         networkConfiguration={
-            'awsvpcConfiguration': {
-                'subnets': [
-                    'subnet-9e7c0fc1',
+            "awsvpcConfiguration": {
+                "subnets": [
+                    "subnet-9e7c0fc1",
                 ],
-                'assignPublicIp': 'ENABLED',
-                'securityGroups': ["sg-0bfa5becbde957cd3"]
+                "assignPublicIp": "ENABLED",
+                "securityGroups": SECURITYGROUP,
             }
-        }
-        )
+        },
+        overrides={
+            "containerOverrides": [
+                {
+                    "name": container_name,
+                    "command": [
+                        "--slug",
+                        start_slug,
+                        "--num_threads",
+                        num_threads,
+                        "--depth",
+                        str(depth),
+                        "--dic",
+                        dic,
+                    ],
+                }
+            ]
+        },
+    )
 
 
 def wait():
     pass
+
+
 def write():
     pass
+
+
 def GetSlug():
     pass
+
+
 def Deploy(num_workers):
     for i in range(num_workers):
         slug = GetSlug()
@@ -52,5 +97,6 @@ def Deploy(num_workers):
     wait()
 
     write()
-    
-    
+
+
+print(type(SECURITYGROUP), SECURITYGROUP)
